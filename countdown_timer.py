aece0ee0f5c33f83fcda2c0389d104acb6cd43a5
mukesh9342472953 <mukeshceo2030@gmail.com>
2025-07-12 12:39:54 +0530

    Update countdown_timer.py

--- countdown_timer.py
+++ countdown_timer.py
@@ -2,7 +2,13 @@ import time
 
 def countdown_timer(seconds):
     while seconds:
-        mins, secs = divmod(seconds)
+        mins, secs = divmod(seconds, 60)
+        timer = f'{mins:02d}:{secs:02d}'
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up!")
 
 if __name__ == "__main__":
     try:
